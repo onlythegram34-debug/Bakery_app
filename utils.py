f646b226a024bccf8e04303f125cdd599facd38c
onlythegram34-debug <onlythegram34@gmail.com>
2026-06-08 13:29:26 +0200

    Add role_required decorator for access control

--- utils.py
+++ utils.py
@@ -1,1 +1,14 @@
+from functools import wraps
+from flask import flash, redirect, url_for
+from flask_login import current_user
 
+def role_required(*roles):
+    def decorator(f):
+        @wraps(f)
+        def decorated_function(*args, **kwargs):
+            if current_user.role not in roles:
+                flash('You do not have permission to access this page.', 'danger')
+                return redirect(url_for('index'))
+            return f(*args, **kwargs)
+        return decorated_function
+    return decorator
